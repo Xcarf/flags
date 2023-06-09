c7bd3b4d82af289ef33d81a63a5036cb5b611784
Ycarf <114652643+Ycarf@users.noreply.github.com>
2023-06-09 11:00:51 +0200

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,52 @@
+let a = 10;
+console.log(a)   
+var clicks = 0;   
+//document.designMode = "on";
+
+
+let AnnoyingButton = document.getElementById("testId");
+console.log(AnnoyingButton)
+AnnoyingButton.style.background="blue";
+
+ 
+
+function TestFunc()
+{
+    AnnoyingButton.style.background="red";
+    AnnoyingButton.innerHTML="Hehehehehehehehehe";
+    alert("Why")
+} 
+
+function SearchLog(Valuue)
+{
+    let Boards = document.getElementsByClassName("Infoboard")
+    for (let i = 0; i < Boards.length; i++) 
+            if (Boards[i].id.toLowerCase().includes(Valuue.toLowerCase())){
+                Boards[i].style.display = null; //   "inline-block";
+            } else {
+                Boards[i].style.display = "none";
+            }
+}
+
+
+function ShowMore(Id)
+    {
+    let MoreInfoBoard = document.getElementById(Id + "mi")
+    console.log("3")
+    console.log(MoreInfoBoard)
+    if(MoreInfoBoard.classList.contains("Disapear")){
+        MoreInfoBoard.classList.remove("Disapear");
+        MoreInfoBoard.classList.add("Apear");
+        document.getElementById(Id).innerHTML = "-";
+    } else {    
+        MoreInfoBoard.classList.remove("Apear");
+        MoreInfoBoard.classList.add("Disapear");
+        document.getElementById(Id).innerHTML = "+";
+    }}
+
+
+    function aClick()
+        clicks = clicks + 1;
+        document.getElementById("numb").innerHTML = clicks
+        
+    
